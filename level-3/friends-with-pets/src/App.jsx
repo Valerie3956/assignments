899622ac68586ce01dcfe4a603f240efd206a25b
Valerie Smith <valerie.plante.11@gmail.com>
2023-10-02 15:23:08 -0400

    some updates to FWP

--- level-3/friends-with-pets/src/App.jsx
+++ level-3/friends-with-pets/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import FriendsList from "./Components/FriendsList.jsx"
+import Header from "./Components/Header.jsx"
+import Footer from "./Components/Footer.jsx"
 
 export default function App() {
   return (
    <div className = "friends--container">
+    <Header />
     <FriendsList />
+    <Footer />
    </div>
   )
 }
---
+++ level-3/friends-with-pets/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return(
+    <div className = "header">
+    <img src = "./blue-cat.jpg"></img>
+    <img src = "yellow-dog.jpg"></img>
+    <img src = "green-dog.jpg"></img>
+    <img src = "red-dog.jpg"></img>
+    <img src = "dark-cat.jpg"></img>
+    </div>
+    )
+}
