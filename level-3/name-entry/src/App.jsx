d6b5ec935929fe5cf9c7cdebc1b7e71f20ac7add
Valerie Smith <valerie.plante.11@gmail.com>
2023-10-05 11:00:45 -0400

    name entry

---
+++ level-3/name-entry/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import React from "react"
+import Name from "./Components/Name.jsx"
+
+// create state and set it to an empty array
+// create form, when submitted pushes the new data to state
+// create component that maps through state and displays each string as an H1
+
+
+const namesArr = []
+
+
+
+export default function App() {
+
+
+
+const [formData, setFormData] = React.useState({
+  name : ""
+})
+
+
+function handleChange(event){
+  const {name, value} = event.target
+  setFormData(prevFormData => {
+    return {
+      ...prevFormData,
+      [name] : value
+    }
+  })
+}
+
+function handleSubmit(event){
+  event.preventDefault()
+  namesArr.push(formData)
+  event.target.reset()
+  console.log(namesArr)
+
+}
+
+const nameComponent = namesArr.map(x => {
+  return <Name name = {x.name} />
+})
+
+  return (
+    <>
+   <form className = "form" id = "form" onSubmit = {handleSubmit}>
+    <input 
+    className = "input"
+    type = "text"
+    placeholder = "type name here"
+    name = "name"
+    value = {formData.name}
+    onChange = {handleChange}
+    />
+    <button className = "button">SUBMIT</button>
+   </form>
+   {nameComponent}
+   </>
+  )
+}
+
---
+++ level-3/name-entry/src/Components/Name.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Name(props){
+    return(
+        <div >
+        <h1 className = "render">{props.name}</h1>
+        </div>
+    )
+}
