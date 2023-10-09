f98b72da73b73edfb2ca0492900cc1852de0b7ae
Valerie Smith <valerie.plante.11@gmail.com>
2023-10-08 22:50:14 -0400

    capstone almost there

--- level-3/capstone/src/App.jsx
+++ level-3/capstone/src/App.jsx
@@ -6,10 +6,12 @@ import Meme from "./Components/Meme.jsx"
 export default function App() {
 
   const [images, setImages] = React.useState([])
+  const [count, setCount] = React.useState(0)
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
-    image: ""
+    image: "./placeholder.jpg",
+    id: count
   })
   const [memeList, setMemeList] = React.useState([])
 
@@ -18,7 +20,6 @@ export default function App() {
       .then(response => setImages(response.data.data.memes))
       .catch(err => console.log(err))
   }, [])
-
 
 
   function generateImage(event) {
@@ -45,57 +46,85 @@ export default function App() {
 
   function submitMeme(event) {
     event.preventDefault()
-    memeList.push(meme)
+    const newMeme = {
+      topText: meme.topText,
+      bottomText: meme.bottomText,
+      image: meme.image,
+      id: count
+    }
+    setMemeList([...memeList, newMeme])
+    setCount(count => count + 1)
     setMeme({
       topText: "",
       bottomText: "",
-      image: ""
+      image: "./placeholder.jpg",
+      id: count
     })
   }
 
   const memes = (memeList.map(x => {
-    return <Meme 
-    topText = {x.topText}
-    bottomText = {x.bottomText}
-    image = {x.image}
-    id = {memeList.indexOf(x)}
-    handleEdit = {console.log("edit")}
-    handleDelete = {console.log("delete")}
+    return <Meme
+      key={x.id}
+      topText={x.topText}
+      bottomText={x.bottomText}
+      image={x.image}
+      id={x.id}
+      handleEdit={edit}
+      handleDelete={del}
     />
   }))
 
+  function del(id) {
+    const newMemeList = memeList.filter(arr => arr.id !== id)
+    setMemeList(newMemeList)
+  }
+
+  function edit(id) {
+    const memeToEdit = memeList.find(arr => arr.id === id)
+    setMeme({
+      topText: memeToEdit.topText,
+      bottomText: memeToEdit.bottomText,
+      image: memeToEdit.image,
+      id: memeToEdit.id
+    })
+    // const newMemeList = memeList.filter(arr => arr.id !== id)
+    // setMemeList(newMemeList)
+  }
+
 
   return (
-    <div className="main">
-      <div className="wrap">
-        <button className="button" onClick={generateImage}>Get New Image</button>
-        <form className="form" onSubmit={submitMeme}>
-          <input
-            type="text"
-            value={meme.topText}
-            name="topText"
-            placeholder="Top Text"
-            className="input"
-            onChange={handleChange}
-          ></input>
-          <input
-            type="text"
-            value={meme.bottomText}
-            name="bottomText"
-            placeholder="Bottom Text"
-            className="input"
-            onChange={handleChange}
-          ></input>
-          <button>SUBMIT MEME</button>
-        </form>
-      </div>
-      <div className="meme">
-        <img src={meme.image} className="image" />
-        <h2 className = "meme--text top">{meme.topText}</h2>
-        <h2 className = "meme--text bottom">{meme.bottomText}</h2>
+    <>
+      <div className="main">
+        <div className="wrap">
+          <button className="button" onClick={generateImage}>Get New Image</button>
+          <form className="form" onSubmit={submitMeme}>
+            <input
+              type="text"
+              value={meme.topText}
+              name="topText"
+              placeholder="Top Text"
+              className="input"
+              onChange={handleChange}
+            ></input>
+            <input
+              type="text"
+              value={meme.bottomText}
+              name="bottomText"
+              placeholder="Bottom Text"
+              className="input"
+              onChange={handleChange}
+            ></input>
+            <button>SUBMIT MEME</button>
+          </form>
+        </div>
+        <div className="meme">
+          <img src={meme.image} className="image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
       </div>
       {memes}
-    </div>
+    </>
   )
 }
 
--- level-3/capstone/src/Components/Meme.jsx
+++ level-3/capstone/src/Components/Meme.jsx
@@ -1,11 +1,15 @@
 export default function Meme(props) {
-    return (
-        <div className="meme">
+  return (
+    <>
+      <div className="meme--container">
         <img src={props.image} className="image" />
-        <h2 className = "meme--text top">{props.topText}</h2>
-        <h2 className = "meme--text bottom">{props.bottomText}</h2>
-        <button onClick = {props.handleEdit}>EDIT MEME</button>
-        <button onClick = {props.handleDelete}>DELETE MEME</button>
+        <h2 className="meme--text top">{props.topText}</h2>
+        <h2 className="meme--text bottom">{props.bottomText}</h2>
       </div>
-    )
+      <div className="button--container">
+        <button onClick={() => props.handleEdit(props.id)}>EDIT MEME</button>
+        <button onClick={() => props.handleDelete(props.id)}>DELETE MEME</button>
+      </div>
+    </>
+  )
 }
