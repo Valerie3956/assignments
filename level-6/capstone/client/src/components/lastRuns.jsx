6639524ff84eb69d96b90da7ce28d49ca5332452
Valerie Smith <valerie.plante.11@gmail.com>
2023-11-05 21:48:51 -0500

    leaderboard done

--- level-6/capstone/client/src/components/lastRuns.jsx
+++ level-6/capstone/client/src/components/lastRuns.jsx
@@ -1,7 +1,23 @@
 import React, { useState, useContext } from 'react'
-
+import axios from 'axios'
+import { UserContext } from '../context/userContext'
 
 export default function LastRuns(props){
+
+    const {userAxios} = useContext(UserContext)
+    
+    const [runs, setRuns] = useState([])
+    
+    useEffect(() => {
+        userAxios.get("api/run/getAll")
+        .then(res => setRunners(res.data))
+        .catch(err => console.log(err.response.data.errMsg))
+    }, [])
+
+    const sortedLastRuns = runners.sort((a, b) => new Date(b.lastRun[date]) - new Date(a.lastRun[date]))
+
+    console.log(sortedRunners)
+
     return(
         <div className = "main">
             <h1>Last Runs</h1>
--- level-6/capstone/client/src/components/leaderboard.jsx
+++ level-6/capstone/client/src/components/leaderboard.jsx
@@ -1,10 +1,29 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios'
+import { UserContext } from '../context/userContext'
+import Runner from './runner'
 
 
 export default function Leaderboard(props){
+    
+    const {userAxios} = useContext(UserContext)
+    
+    const [runners, setRunners] = useState([])
+    
+    useEffect(() => {
+        userAxios.get("api/run/getAll")
+        .then(res => setRunners(res.data))
+        .catch(err => console.log(err.response.data.errMsg))
+    }, [])
+
+    const sortedRunners = runners.sort((a, b) => b.totalMiles - a.totalMiles)
+
+    console.log(sortedRunners)
+
     return(
         <div className = "main">
-            <h1>Leaderboard</h1>
+            <h1 className = "title">Leaderboard</h1>
+            {sortedRunners.map(runner => <Runner {...runner} key = {runner._id}/>)}
         </div>
     )
 }
